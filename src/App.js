caab7f0c6a084f3545106e5e556505c1c908c43d
Gail Foad <gail.foad@thenavpeople.co.uk>
2023-02-12 18:55:32 +0000

    eventlistener

--- src/App.js
+++ src/App.js
@@ -2,35 +2,22 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
 
-  const [resourceType, setResourceType] = useState('posts');
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  const [items, setItems] = useState([]);
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  }
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-      .then(response => response.json())
-      .then(json => {
-          console.log(json);
-          setItems(json);
-          console.log('items: ', items);
-        }
-      );
-     
-  }, [resourceType , items])
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      console.log('goodbye');
+    }
+  },[]);
 
   return (
-   <>
-   <div>
-      <button onClick={() => setResourceType('posts')}>Posts</button>
-      <button onClick={() => setResourceType('users')}>Users</button>
-      <button onClick={() => setResourceType('comments')}>Comments</button>
-   </div>
-   
-    <h1>{resourceType}</h1>
-
-    {items.map(item => {
-      return <pre>{JSON.stringify(item)}</pre>})}
-   </>
+  <div>{windowWidth}</div>
   );
 }
 
