1d7306fee53e716b79fc548b08c5d52039e21fc6
Gail Foad <gail.foad@thenavpeople.co.uk>
2023-02-12 17:23:20 +0000

    useState()

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [bob, setBob] = useState(0);
+
+  console.log('🐈 ~ file: App.js:7 ~ bob', bob);
+
+  function decrementCount() {
+    setBob(beforeVal => beforeVal - 1);
+  }
+
+  function augumentCount() {
+    setBob(beforeVal => beforeVal + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <button onClick={decrementCount}>-</button>&nbsp;
+    <span>{bob}</span>&nbsp;
+    <button onClick={augumentCount}>+</button>
+   </>
   );
 }
 
