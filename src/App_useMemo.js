40b3163bf98bcb1cee87638deb2b2944a68c0af5
Gail Foad <gail.foad@thenavpeople.co.uk>
2023-02-14 13:17:07 +0000

    Backup useMemo()

---
+++ src/App_useMemo.js
@@ -0,0 +1,43 @@
+import React, { useState, useMemo, useEffect } from 'react';
+
+function App() {
+
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+  
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number)
+  }, [number]);
+
+  const themeStyles = useMemo(() =>  {
+    return {
+    backgroundColor: dark ? 'black' : 'white',
+    color: dark ? 'white' : 'black'     
+  }
+  }, [dark]);
+
+  useEffect(() => {
+    console.log('Theme changed');
+  }, [themeStyles]);
+
+  // const themeStyles = {
+  //   backgroundColor: 'red',
+  //   color:'white'     
+  // }
+ 
+  return (
+   <>
+   <input type="number" value={number} onChange={e => setNumber(parseInt(e.target.value))} />
+    <button onClick={() => setDark(prevDark => !prevDark)}>Toggle Theme</button>
+    <div style={themeStyles}>{doubleNumber}</div>
+   </>
+  );
+}
+
+function slowFunction(num) {
+  console.log('Calling slow function');
+  for (let i = 0; i <= 1000000000; i++) {}
+  return num * 2;
+}
+
+export default App;
